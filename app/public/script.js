b97f2cb17163403f5a6ed967f8729465897f2711
Riley Griffin <rigriffin241@gmail.com>
2018-09-11 19:45:10 -0400

    added and started script.js page, so i dont have to use jquery for ajax calls

---
+++ app/public/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const getForm = () => {
+  const elements = document.forms[].elements;
+};
+
+const goSubmit = () => {
+
+    const submitButton = document.getElementById('submit');
+
+
+};
