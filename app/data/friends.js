14f6473c87f9ff945a9c8ed66875285387b83b72
Riley Griffin <rigriffin241@gmail.com>
2018-09-10 23:35:56 -0400

    finally working. Figured out the error was I was setting bodyParser.urlencode to false instead of true.... Man I love finding bugs!

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,29 @@
 const bodyParser = require('body-parser');
-const dataArray = [];
+
+const dataArray = [{}];
 
 const throwJson = (app) => {
     //confirm app is what we want to push
 
-    dataArray.push(app);
-
     app.get('/api/friends', function(req, res){
         res.send(dataArray);
+    });
+
+    const jsonParse = bodyParser.json();
+    const urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+    app.post('/api/friends', urlencodedParser, function (req, res) {
+        if(!req.body) return res.sendStatus(400);
+        res.send(req.body);
+        console.log('sent');
+        dataArray.push(req.body);
+    });
+
+    app.post('/api/friends', jsonParse, function(req,res){
+        if(!req.body) return res.sendStatus(400);
+        res.send(req.body);
+        console.log('sent');
+        dataArray.push(req.body);
     });
 
 
--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const request = require('supertest');
 const bodyParser = require('body-parser');
 
 
@@ -9,25 +8,10 @@ const api = (app) => {
         showApi(app);
     //this will return json for
     app.get('/api/friends', function(req, res){
-        res.json(path.join(showApi()));
+        res.json(path.join(showApi));
     });
 
-    app.use(bodyParser);
-    app.use(request);
 
-    app.post('/api/friends', function(req,res){
-        console.log(req);
-        res.status(200).json(showApi(/*requestbody*/));
-    });
-
-    request(app)
-        .post('/api/friends')
-        .expect('Content-Type', /json/)
-        .expect('Content-Length', '30')
-        .expect(200)
-        .end(function(err, res){
-            if(err) throw err;
-        });
 };
 
 module.exports = api;
