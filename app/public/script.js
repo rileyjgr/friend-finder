25f4880a7aab2ac09115daffffd19ce5b3b142b0
Riley Griffin <rigriffin241@gmail.com>
2018-09-11 20:55:07 -0400

    working script.js without post request

--- app/public/script.js
+++ app/public/script.js
@@ -1,47 +1,50 @@
 'use strict';
 
+const personArray = [{}];
+const answersArray = [{}];
+
+//declares submit button
+const submitButton = document.getElementById('submit');
+
+//pushes to person array, on submit button click
 const getNaPic = () => {
     const name = document.getElementById('name').value;
     const photo = document.getElementById('photo').value;
 
-    const person = [name, photo];
-    const whoIs = person.value;
-    console.log(whoIs);
+
+    const whoIs = [name, photo];
+    personArray.push(whoIs);
 };
 
+
+// pushes answers of form to answerArray
 
 const getAnswers = () => {
-    const q1 = document.getElementById('q1');
-    const q2 = document.getElementById('q2');
-    const q3 = document.getElementById('q3');
-    const q4 = document.getElementById('q4');
-    const q5 = document.getElementById('q5');
-    const q6 = document.getElementById('q6');
-    const q7 = document.getElementById('q7');
-    const q8 = document.getElementById('q8');
-    const q9 = document.getElementById('q9');
-    const q10 = document.getElementById('q10');
+    const q1 = document.getElementById('q1').value;
+    const q2 = document.getElementById('q2').value;
+    const q3 = document.getElementById('q3').value;
+    const q4 = document.getElementById('q4').value;
+    const q5 = document.getElementById('q5').value;
+    const q6 = document.getElementById('q6').value;
+    const q7 = document.getElementById('q7').value;
+    const q8 = document.getElementById('q8').value;
+    const q9 = document.getElementById('q9').value;
+    const q10 = document.getElementById('q10').value;
 
     const questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
-    const value = questions.options[questions.selectedIndex].value;
-    console.log(value);
+
+    answersArray.push(questions);
 
 };
 
-
-
-const goSubmit = () => {
-
-    const submitButton = document.getElementById('submit');
-
-    const data = [getNaPic(), getAnswers()];
+submitButton.addEventListener('click', function(){
+    getNaPic();
+    getAnswers();
+    const data = [personArray, answersArray];
 
     console.log(data);
 
-    submitButton.addEventListener('click', function(){
 
-   });
-
-};
+});
 
 
