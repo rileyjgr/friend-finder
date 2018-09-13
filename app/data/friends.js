cd2aa9dc17c0fe302329de9e2b66cc73e342d7b2
Riley Griffin <rigriffin241@gmail.com>
2018-09-12 22:07:00 -0400

    getting closer

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,27 +19,81 @@ const throwJson = (app) => {
     // sends the data to dataArray
     app.post('/api/friends', urlencodedParser, parseJson, function (req, res) {
         if(!req.body) return res.sendStatus(400);
-        const match = '';
-        const getMatch = (match) => {
+
+        // const match = '';
+        const getMatch = () => {
             const scores = [];
 
-            for (let i=0; i<1; i++ ) {
-                const userName  = req.body.name;
-                const userScore = req.body.scores;
+            const getSum = () => {
+                for (let i = 0; i < 1; i++) {
+                    const userName = req.body.name;
+                    const userScore = req.body.scores;
 
-                // function to add the scores together
-                const total = (userScore) => {
+                    // function to add the scores together
+                    const total = (userScore) => {
 
-                    let sum = 0;
-                    userScore.forEach((value) => {
-                        sum += parseInt(value);
-                    });
+                        let sum = 0;
+                        userScore.forEach((value) => {
+                            sum += parseInt(value);
+                        });
 
-                    return sum;
-                };
+                        return sum;
+                    };
 
-                scores.push({"name": userName, "score": total(userScore)});
-            }
+                    const finalScore = total(userScore);
+                    scores.push({"name": userName, "score": finalScore});
+
+                    // this will be the part of the algorithm that searches for a similar user.
+                    const findMatch = () => {
+
+                        let findName = "";
+                        let findScore = "";
+
+                        const find = (scores, name) => {
+
+                            for(name.score in scores) {
+
+                                if(scores.name.score = finalScore.split(',').indexOf(scores.name.score)){
+
+                                    findName = scores.name;
+                                    findScore = scores.name.score;
+
+                                    console.log(findName);
+                                    return findName + findScore;
+
+                                }
+                                return 'not found';
+
+                            }
+                            console.log(findName);
+                            console.log(findScore);
+
+                        };
+
+                        const matchName = find(findName);
+                        const matchScore = find(findScore);
+
+                        // console.log(matchName);
+                        // console.log(matchScore);
+
+                        // who we are looking for
+                        const match = {
+
+                            "name": matchName,
+                            "score": matchScore
+                        };
+
+                        console.log('here is your match: ' + match.name + match.score);
+                    };
+
+                    findMatch();
+                }
+
+
+            };
+
+            getSum();
+
 
 
 
